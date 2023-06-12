fd48d21180660187f0d7ae1f027da56e0395a699
mcwolfe2011 <mcwolfe2011@gmail.com>
2023-06-11 20:27:08 -0700

    Create resusable and update Components using props.

--- app/components/Container.js
+++ app/components/Container.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react"
 
-function Container() {
+function Container(props) {
   return (
-    <>
-      
-    </>
+    <div className="container container--narrow py-md-5">
+      {props.children}
+    </div>
   )
 }
 

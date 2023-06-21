d4ab57a66cc9f17bb69471dc3cbb36328282ea69
mcwolfe2011 <mcwolfe2011@gmail.com>
2023-06-21 16:00:27 -0700

    Add Tooltips on Hover for Actions

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -38,7 +38,7 @@ function ViewSinglePost() {
       <div className="d-flex justify-content-between">
           <h2>{post.title}</h2>
           <span class="pt-2">
-            <a href="#" className="text-primary mr-2" title="Edit"><i className="fas fa-edit"></i></a>
+            <a href="#" className="text-primary mr-2" title="Edit"><i className="fas fa-edit"></i></a> {" "}
             <a className="delete-post-button text-danger" title="Delete"><i className="fas fa-trash"></i></a>
           </span>
         </div>

d427661d191934f9ffdbce2b8c3c9db1c4e403aa
mcwolfe2011 <mcwolfe2011@gmail.com>
2023-07-30 15:45:55 -0700

    Quick attention to detail features.

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,60 +1,103 @@
-import React, { useEffect, useState } from "react"
-import Page from './Page'
-import { useParams, Link } from "react-router-dom"
-import Axios from 'axios'
-import LoadingDotsIcon from "./LoadingDotsIcon"
-import ReactMarkdown from 'react-markdown'
+import React, { useEffect, useState } from "react";
+import Page from "./Page";
+import { useParams, Link } from "react-router-dom";
+import Axios from "axios";
+import LoadingDotsIcon from "./LoadingDotsIcon";
+import ReactMarkdown from "react-markdown";
+import NotFound from "./NotFound";
 
 function ViewSinglePost() {
-  const {id} = useParams()
-  const [isLoading, setIsLoading] = useState(true)
-  const [post, setPost] = useState()
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [post, setPost] = useState();
 
   useEffect(() => {
-    const ourRequest = Axios.CancelToken.source()
+    const ourRequest = Axios.CancelToken.source();
 
     async function fetchPost() {
       try {
-        const response = await Axios.get(`/post/${id}`, {cancelToken: ourRequest.token})
-        setPost(response.data)
-        setIsLoading(false)
-      } catch(e) {
-        console.log("There was a problem or the request was cancelled.")
+        const response = await Axios.get(`/post/${id}`, {
+          cancelToken: ourRequest.token
+        });
+        setPost(response.data);
+        setIsLoading(false);
+      } catch (e) {
+        console.log("There was a problem or the request was cancelled.");
       }
     }
-    fetchPost()
+    fetchPost();
     return () => {
-      ourRequest.cancel()
-    }
-  }, [])
+      ourRequest.cancel();
+    };
+  }, []);
 
-  if (isLoading) return (<Page title="..."><LoadingDotsIcon /></Page>)
+  if (!isLoading && !post) {
+    return <NotFound />;
+  }
 
-  const date = new Date(post.createdDate)
-  const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+  if (isLoading)
+    return (
+      <Page title="...">
+        <LoadingDotsIcon />
+      </Page>
+    );
+
+  const date = new Date(post.createdDate);
+  const dateFormatted = `${
+    date.getMonth() + 1
+  }/${date.getDate()}/${date.getFullYear()}`;
 
   return (
     <Page title={post.title}>
       <div className="d-flex justify-content-between">
-          <h2>{post.title}</h2>
-          <span class="pt-2">
-            <Link to={`/post/${post._id}/edit`} className="text-primary mr-2" title="Edit"><i className="fas fa-edit"></i></Link> {" "}
-            <a className="delete-post-button text-danger" title="Delete"><i className="fas fa-trash"></i></a>
-          </span>
-        </div>
+        <h2>{post.title}</h2>
+        <span class="pt-2">
+          <Link
+            to={`/post/${post._id}/edit`}
+            className="text-primary mr-2"
+            title="Edit"
+          >
+            <i className="fas fa-edit"></i>
+          </Link>{" "}
+          <a className="delete-post-button text-danger" title="Delete">
+            <i className="fas fa-trash"></i>
+          </a>
+        </span>
+      </div>
 
       <p className="text-muted small mb-4">
         <Link to={`/profile/${post.author.username}`}>
           <img className="avatar-tiny" src={post.author.avatar} />
         </Link>
-        Posted by <Link to={`/profile/${post.author.username}`}>{post.author.username}</Link> {dateFormatted}
+        Posted by{" "}
+        <Link to={`/profile/${post.author.username}`}>
+          {post.author.username}
+        </Link>{" "}
+        {dateFormatted}
       </p>
 
       <div className="body-content">
-        <ReactMarkdown children={post.body} allowedElements={["p", "br", "strong", "em", "h1", "h2", "h3", "h4", "h5", "h6", "ul", "ol", "li"]}/>
+        <ReactMarkdown
+          children={post.body}
+          allowedElements={[
+            "p",
+            "br",
+            "strong",
+            "em",
+            "h1",
+            "h2",
+            "h3",
+            "h4",
+            "h5",
+            "h6",
+            "ul",
+            "ol",
+            "li"
+          ]}
+        />
       </div>
     </Page>
-  )
+  );
 }
 
-export default ViewSinglePost
+export default ViewSinglePost;

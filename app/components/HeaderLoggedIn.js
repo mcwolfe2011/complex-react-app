2e09d0e2a5912801a7a40801bb296e422b117d1b
mcwolfe2011 <mcwolfe2011@gmail.com>
2023-08-03 19:52:08 -0700

    React Transition Group (CSS Transition)

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -2,6 +2,7 @@ import React, { useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import DispatchContext from "../DispatchContext";
 import StateContext from "../StateContext";
+// import { Tooltip } from "react-tooltip";
 
 function HeaderLoggedIn(props) {
   const appDispatch = useContext(DispatchContext);
@@ -19,22 +20,25 @@ function HeaderLoggedIn(props) {
   return (
     <div className="flex-row my-3 my-md-0">
       <a
+        data-tooltip-id="search"
+        data-tooltip-content="Search"
         onClick={handleSearchIcon}
         href="#"
         className="text-white mr-2 header-search-icon"
       >
         <i className="fas fa-search"></i>
       </a>
+      {/* <ReactTooltip place="bottom" id="search" className="custom-tooltip" /> */}{" "}
       <span className="mr-2 header-chat-icon text-white">
         <i className="fas fa-comment"></i>
         <span className="chat-count-badge text-white"> </span>
-      </span>
+      </span>{" "}
       <Link to={`/profile/${appState.user.username}`} className="mr-2">
         <img className="small-header-avatar" src={appState.user.avatar} />
-      </Link>
+      </Link>{" "}
       <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
-      </Link>
+      </Link>{" "}
       <button onClick={handleLogout} className="btn btn-sm btn-secondary">
         Sign Out
       </button>

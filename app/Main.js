1fac13a6fd1ed1fcc4e4215f5ecdc9fd7122f7d0
mcwolfe2011 <mcwolfe2011@gmail.com>
2023-09-12 17:04:35 -0700

    Push Front-End to the Web

--- app/Main.js
+++ app/Main.js
@@ -4,7 +4,9 @@ import { useImmerReducer } from "use-immer";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CSSTransition } from "react-transition-group";
 import Axios from "axios";
-Axios.defaults.baseURL = process.env.BACKENDURL || "";
+Axios.defaults.baseURL =
+  process.env.BACKENDURL ||
+  "https://myreactbackendcomplexsocialmediaapp.onrender.com";
 
 import StateContext from "./StateContext";
 import DispatchContext from "./DispatchContext";
